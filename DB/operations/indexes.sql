26574427a378a61d349538af2b1124f5d5eef5cd
yoszimitsu <cotam07@gmail.com>
2020-11-24 14:16:58 +0100

    add date indexes

--- DB/operations/indexes.sql
+++ DB/operations/indexes.sql
@@ -6,3 +6,9 @@ CREATE UNIQUE INDEX supervisor_idx ON company.supervisor (name, surname);
 CREATE  INDEX invest_name_idx ON company.investment (name);
 
 CREATE  INDEX sub_invest_name_idx ON company.sub_investment (name);
+
+CREATE  INDEX sub_investment_start_date ON company.sub_investment (start_date);
+CREATE  INDEX sub_investment_end_date ON company.sub_investment (end_date);
+
+CREATE  INDEX investment_start_date ON company.investment (start_date);
+CREATE  INDEX investment_end_date ON company.investment (end_date);
